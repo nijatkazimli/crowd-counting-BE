630c2972fd646ca63c07a2af474902c215d9d34c
Nijat Kazimli <kazimli.n@pg.com>
2024-11-17 14:34:51 +0100

    temp fix

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ import os
 load_dotenv()
 
 class Config:
-    AZURE_STORAGE_CONNECTION_STRING = os.environ.get('AZURE_STORAGE_CONNECTION_STRING')
-    AZURE_STORAGE_CONTAINER = os.environ.get('AZURE_STORAGE_CONTAINER')
+    AZURE_STORAGE_CONNECTION_STRING = 'AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://azuriteDocker:10000/devstoreaccount1;QueueEndpoint=http://azuriteDocker:10001/devstoreaccount1;TableEndpoint=http://azuriteDocker:10002/devstoreaccount1'
+    AZURE_STORAGE_CONTAINER = 'crowd-counting'
 
 class DevelopmentConfig(Config):
     DEBUG = True
