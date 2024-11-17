0c6cb4e3c83c9cbb55a99afee3cc60a8c4b90339
Nijat Kazimli <kazimli.n@pg.com>
2024-11-17 14:21:04 +0100

    config for docker-compose

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,15 @@
 from flask import request, jsonify
 from app import app
+import re
 from app.azureUtils import get_blob_service_client, upload_file_to_blob
 from app.database import get_db_connection
+
+def replace_az_docker(url: str) -> str:
+    regex = r"^http://azuriteDocker:10000/"
+    pattern = re.compile(regex)
+    if pattern.search(url):
+        return pattern.sub("http://localhost:10000/", url)
+    return url
 
 @app.after_request
 def handle_options(response):
@@ -55,6 +63,7 @@ def upload_file():
     blob_service_client = get_blob_service_client(connection_str)
     try:
         file_url = upload_file_to_blob(file, container_name, blob_service_client)
+        file_url = replace_az_docker(file_url)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
     
