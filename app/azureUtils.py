0ee98edbe307da1e86526f0423654a9b0fa7c312
Nijat Kazimli <kazimli.n@pg.com>
2024-11-29 23:08:35 +0100

    new azure utils to upload and download

--- app/azureUtils.py
+++ app/azureUtils.py
@@ -1,4 +1,6 @@
 from azure.storage.blob import BlobServiceClient, PublicAccess
+from urllib.parse import urlparse
+import os
 
 def get_blob_service_client(connection_str):
     return BlobServiceClient.from_connection_string(connection_str)
@@ -19,3 +21,24 @@ def upload_file_to_blob(file, container_name, blob_service_client):
     blob_client = blob_service_client.get_blob_client(container=container_name, blob=file.filename)
     blob_client.upload_blob(file, overwrite=True)
     return blob_client.url
+
+def upload_file_path_to_blob(file_path, container_name, blob_service_client):
+    create_container_if_not_exists(blob_service_client, container_name)
+    file_name = os.path.basename(file_path)
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=file_name)
+    with open(file_path, "rb") as file:
+        blob_client.upload_blob(file, overwrite=True)
+    return blob_client.url
+
+def download_file_from_blob(url, blob_service_client, download_path, container_name):
+    parsed_url = urlparse(url)
+    path_parts = parsed_url.path.strip('/').split('/')
+    blob_name = path_parts[-1]  # Last part is the file name
+    os.makedirs(download_path, exist_ok=True)
+    download_file_path = os.path.join(download_path, blob_name)
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+
+    with open(download_file_path, "wb") as download_file:
+        download_file.write(blob_client.download_blob().readall())
+
+    return download_file_path
