21ca6c36d43de1c268991395948dd10d1549adba
Nijat Kazimli <kazimli.n@pg.com>
2024-11-29 20:06:00 +0100

    dynamically load the models into db

--- migrations/init_db.py
+++ migrations/init_db.py
@@ -2,6 +2,7 @@ import sqlite3
 import os
 
 os.makedirs('instance', exist_ok=True)
+os.makedirs('models', exist_ok=True)
 
 def init_db():
     conn = sqlite3.connect('instance/crowd-counting.db')
@@ -25,14 +26,10 @@ def init_db():
         )
     ''')
     
-    # TODO: Daniel to add actual models
-    models = [
-        ('Model A',),
-        ('Model B',),
-        ('Model C',)
-    ]
+    model_files = [f for f in os.listdir('models') if os.path.isfile(os.path.join('models', f))]
+    model_names = [(os.path.splitext(file)[0],) for file in model_files]
     
-    for model in models:
+    for model in model_names:
         c.execute('SELECT COUNT(*) FROM models WHERE name=?', model)
         if c.fetchone()[0] == 0:
             c.execute('INSERT INTO models (name) VALUES (?)', model)
